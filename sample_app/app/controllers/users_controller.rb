42d02850473eac4729c104f3f989b12185c2c1aa
songsong <songsong@Gialib001deMini.lan>
2016-08-01 15:55:14 +0800

    finish lsactions:edit,update,index,and destroy

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page]).per_page(15)
   end
 
   def new
@@ -12,8 +15,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
   def create
-    @user = User.new(user_params)
     if @user.save
       sign_in @user
       flash[:success] = "注册成功，欢迎加入"
@@ -23,9 +28,45 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "修改成功"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    flash[:success] = "删除成功"
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # Before filters
+  def signed_in_user
+    #redirect_to signin_path, notice:"请先登录" unless signed_in?
+    unless signed_in?
+      store_location
+      flash[:warning] = "请先登录"
+      redirect_to signin_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
+  def admin_user 
+    redirect_to (root_path) unless current_user.admin?
   end
 end
---
+++ sample_app/lib/tasks/sample_data.rake
@@ -0,0 +1,25 @@
+namespace :db do
+  desc "Fill database with sample data"
+  task populate: :environment do
+    94.times do |n|
+      name  = Faker::Name.name
+      email = "example-#{n+1}@railstutorial.org"
+      password  = "aaaaaa"
+      User.create!(name: name,
+                   email: email,
+                   password: password,
+                   password_confirmation: password,
+                   admin: false)
+    end
+  end
+    
+  desc "add a admin user"
+  task add_admin: :environment do
+    User.create!(name: "管理员",
+                         email: "shi1400875096@outlook.com",
+                         password: "Xinxin1hao",
+                         password_confirmation: "Xinxin1hao",
+                         admin: true)
+  end
+
+end
